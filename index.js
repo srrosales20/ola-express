a60ddfe88666044219a957e8311b2569341987e6
Sonia Rosales <103076929+srrosales20@users.noreply.github.com>
2022-08-10 16:02:07 +0000

    this adds express

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Ola from express!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+// var http = require("http");
+// http.createServer(function(req,res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Aloha world');
+// }).listen(process.env.PORT || 3000);
